c5fa84f6c6458e12f9fd8a0b9856411d5e26bfdf
Daniel García Narvaez <narvaezad@uninorte.edu.co>
2025-05-21 01:48:50 -0500

    Json 2

--- src/airport/core/views/PanelRound.java
+++ src/airport/core/views/PanelRound.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package airport;
 
 import java.awt.Color;
@@ -12,10 +9,6 @@ import java.awt.geom.Area;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.JPanel;
 
-/**
- *
- * @author jeffr
- */
 public class PanelRound extends JPanel {
 
     public int getRadius() {
