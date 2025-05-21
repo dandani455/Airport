764071fe692aed485003e9d7e7a5dd3213eb01b8
edoesper <espere@uninortre.edu.co>
2025-05-20 22:17:18 -0500

    Reapply "Merge pull request #1 from dandani455/dan"
    This reverts commit 307dc26f5efd671baafadcddb3550f84df5772d3.

---
+++ src/airport/core/models/storage/adapters/PassengerAdapter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package airport.core.models.storage.adapters;
+
+/**
+ *
+ * @author dales
+ */
+public class PassengerAdapter {
+    
+}
